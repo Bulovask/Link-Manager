c80ba6cba853355ff265dea183b100aaca060e99
ageu <ageuassuncaosimoesgalindo@gmail.com>
2022-04-22 21:03:33 -0300

    commit 2

--- src/js/controllers/rootFolderCtrl.js
+++ src/js/controllers/rootFolderCtrl.js
@@ -1,14 +1,23 @@
 angular.module("LinkManager").controller("rootFolderCtrl", function($scope, Link, Folder) {
 	const params = new URLSearchParams(window.location.search);
-	$scope.tree = [new Link("404", "http://www.bink.com"), new Folder("Sites", [new Link("google", "https://www.google.com"), new Folder("Pasta", [new Link("fim")])])];
-	$scope.pwd = params.get("pwd") || "";
+	
+	$scope.tree = [
+		new Folder("Sites", [
+			new Folder("a", [
+				new Folder("b", [
+					new Folder("c", [
+						new Folder("d", [
+							new Folder("e", [
+								new Link("f")
+							])
+						])
+					])
+				])
+			])
+		])
+	];
+	
 	$scope.currentFolder = $scope.tree;
-	if($scope.pwd != "") {
-		const indexs = $scope.pwd.split("/");
-		for(let i = 0; i < indexs.length; i++) {
-			$scope.currentFolder = $scope.currentFolder[indexs[i]].referenceList;
-		}
-	}
 	
 	$scope.itemAction = (item, currentIndex) => {
 		switch(item.type) {
@@ -17,9 +26,13 @@ angular.module("LinkManager").controller("rootFolderCtrl", function($scope, Link
 				else alert("Link Vazio");
 			break;
 			case "folder":
-				const url = $scope.pwd == "" ? String("?pwd=" + currentIndex) : "?pwd=" + $scope.pwd + "/" + currentIndex;
-				window.open(url, "_self");
+				item.parent = $scope.currentFolder;
+				$scope.currentFolder = item.referenceList;
 			break;
 		}
+	}
+	
+	$scope.back = () => {
+		$scope.currentFolder = $scope.currentFolder.parent.referenceList;
 	}
 });
