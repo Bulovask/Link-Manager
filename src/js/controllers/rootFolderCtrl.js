0b6487cd79084b1eb736ae75535f5536f0b9ceda
Bulovask <ageuassuncaosimoesgalindo@gmail.com>
2022-05-03 14:48:45 -0300

    add newItem sources

--- src/js/controllers/rootFolderCtrl.js
+++ src/js/controllers/rootFolderCtrl.js
@@ -1,25 +1,10 @@
 angular.module("LinkManager").controller("rootFolderCtrl", function($scope, Link, Folder) {
-	const params = new URLSearchParams(window.location.search);
-	
-	$scope.tree = [
-		new Folder("Sites", [
-			new Folder("a", [
-				new Folder("b", [
-					new Folder("c", [
-						new Folder("d", [
-							new Folder("e", [
-								new Link("f")
-							])
-						])
-					])
-				])
-			])
-		])
-	];
-	
+	//object-list-container
+	$scope.tree = new Folder("root", []);
+	$scope.path = [$scope.tree];
 	$scope.currentFolder = $scope.tree;
 	
-	$scope.itemAction = (item, currentIndex) => {
+	$scope.itemAction = (item, index) => {
 		switch(item.type) {
 			case "link":
 				if(item.url) window.open(item.url, "_blank");
@@ -27,12 +12,48 @@ angular.module("LinkManager").controller("rootFolderCtrl", function($scope, Link
 			break;
 			case "folder":
 				item.parent = $scope.currentFolder;
-				$scope.currentFolder = item.referenceList;
+				$scope.currentFolder = item;
+				$scope.path.push(item);
 			break;
 		}
 	}
 	
 	$scope.back = () => {
-		$scope.currentFolder = $scope.currentFolder.parent.referenceList;
+		if($scope.currentFolder.parent) {
+			$scope.currentFolder.parent.lastChildVisited = $scope.currentFolder;
+			$scope.currentFolder = $scope.currentFolder.parent;
+			$scope.path.pop();
+		}
+	}
+	
+	$scope.next = () => {
+		if($scope.currentFolder.lastChildVisited) {
+			$scope.currentFolder = $scope.currentFolder.lastChildVisited;
+			$scope.path.push($scope.currentFolder);
+		}
+	}
+	
+	$scope.go = (folder, index) => {
+		$scope.path.splice(index + 1);
+		$scope.currentFolder = folder;
+	}
+	
+	//new-item-container
+	$scope.newItemContainerVisible = false;
+	$scope.itemEdit = {};
+	$scope.itemEdit.type = 'link';
+	
+	$scope.addItem = item => {
+		let newItem;
+		
+		if(item.type == "link" && item.url) newItem = new Link(item.name, item.url);
+		else if(item.type == "folder") newItem = new Folder(item.name, []);
+		
+		if(newItem && item.name) {
+			$scope.currentFolder.referenceList.push(newItem);
+			$scope.newItemContainerVisible = false;
+			$scope.itemEdit = {};
+			$scope.itemEdit.type = 'link';
+		}
 	}
 });
--- src/js/services/itemsFactory.js
+++ src/js/services/itemsFactory.js
@@ -2,6 +2,7 @@ angular.module("LinkManager").factory("Link", function() {
 	return function(name, url, image) {
 		this.type = "link";
 		this.name = name;
+		this.description = "";
 		this.url = url;
 		this.color = "#ccf";
 		this.image = image;
@@ -12,6 +13,7 @@ angular.module("LinkManager").factory("Folder", function() {
 	return function(name, list=[], image) {
 		this.type = "folder";
 		this.name = name;
+		this.description = "";
 		this.referenceList = list;
 		this.color = "#cfc";
 		this.image = image;
